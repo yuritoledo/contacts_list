452cc8c7986a536d7febb5b80edc3a5ab5a3785d
Yuri Toledo <yuriwtoledo@gmail.com>
2019-12-10 06:59:59 -0300

    feat: Create callback contact (upsert)

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:contacts_list/helpers/contact.dart';
+import 'package:contacts_list/ui/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -15,11 +16,33 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    helper.getAllContacts().then((f) {
-      setState(() {
-        list = f;
-      });
-    });
+    _getAllContacts();
+  }
+
+  void _getAllContacts() async {
+    final allContacts = await helper.getAllContacts();
+
+    if (allContacts != null) {
+      setState(() => list = allContacts);
+    }
+  }
+
+  Future _showContact({Contact contact}) async {
+    final callbackContact = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => ContactForm(
+                  contact: contact,
+                )));
+
+    if (callbackContact != null) {
+      if (contact != null) {
+        await helper.updateContact(contact);
+      } else {
+        await helper.saveContact(callbackContact);
+      }
+      _getAllContacts();
+    }
   }
 
   @override
@@ -31,56 +54,56 @@ class _HomeState extends State<Home> {
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: _showContact,
         backgroundColor: Colors.red,
         child: Icon(Icons.add),
       ),
       body: ListView.builder(
         itemCount: list.length,
-        itemBuilder: (context, index) {
-          final item = list[index];
-          return _buildCard(item);
-        },
+        itemBuilder: (context, index) => _buildCard(list[index]),
       ),
     );
   }
 
-  Widget _buildCard(Contact item) {
-    return Padding(
-      padding: EdgeInsets.all(10.0),
-      child: Row(
-        children: <Widget>[
-          Container(
-            width: 80.0,
-            height: 80.0,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                    image: item.img != null
-                        ? FileImage(File(item.img))
-                        : AssetImage('images/avatar.png'))),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  item.name,
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  item.email,
-                  style: TextStyle(fontSize: 20),
-                ),
-                Text(
-                  item.phone,
-                  style: TextStyle(fontSize: 20),
-                ),
-              ],
+  Widget _buildCard(Contact contact) {
+    return GestureDetector(
+      onTap: () => _showContact(contact: contact),
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Row(
+          children: <Widget>[
+            Container(
+              width: 80.0,
+              height: 80.0,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: contact.img != null
+                          ? FileImage(File(contact.img))
+                          : AssetImage('images/avatar.png'))),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    contact.name,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    contact.email,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    contact.phone,
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
