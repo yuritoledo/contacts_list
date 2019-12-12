ba094b05e3635ee6886205e0f9caefffbcbe1684
Yuri Toledo <yuriwtoledo@gmail.com>
2019-12-12 07:03:49 -0300

    Added warning alert when leaving without saving

--- lib/ui/contact_form.dart
+++ lib/ui/contact_form.dart
@@ -14,6 +14,7 @@ class ContactForm extends StatefulWidget {
 
 class _ContactFormState extends State<ContactForm> {
   Contact contact;
+  bool _isDirty = false;
 
   final _nameCtrl = TextEditingController();
   final _emailCtrl = TextEditingController();
@@ -44,55 +45,100 @@ class _ContactFormState extends State<ContactForm> {
     Navigator.pop(context, contact);
   }
 
+  Future<bool> _checkIsDirty() {
+    if (_isDirty) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text("Confirma o cancelamento?"),
+              content: Text('Todos os dados editados serão perdidos'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Sim'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pop(context);
+                  },
+                ),
+                FlatButton(
+                  child: Text('Não'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            );
+          });
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('New contact'),
-        backgroundColor: Colors.red,
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _saveContact,
-        child: Icon(Icons.save),
-        backgroundColor: Colors.red,
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image(
-                image: contact.img == null
-                    ? AssetImage('images/avatar.png')
-                    : FileImage(File(contact.img))),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _nameCtrl,
-                decoration: InputDecoration(
-                    labelText: 'Nome', border: OutlineInputBorder()),
+    return WillPopScope(
+      onWillPop: _checkIsDirty,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('New contact'),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _saveContact,
+          child: Icon(Icons.save),
+          backgroundColor: Colors.red,
+        ),
+        body: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                  image: contact.img == null
+                      ? AssetImage('images/avatar.png')
+                      : FileImage(File(contact.img))),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _nameCtrl,
+                  onChanged: (text) {
+                    setState(() => _isDirty = true);
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Nome',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _emailCtrl,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    labelText: 'Email', border: OutlineInputBorder()),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _emailCtrl,
+                  onChanged: (text) {
+                    setState(() => _isDirty = true);
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Email', border: OutlineInputBorder()),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _phoneCtrl,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                    labelText: 'Telefone', border: OutlineInputBorder()),
-              ),
-            )
-          ],
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _phoneCtrl,
+                  onChanged: (text) {
+                    setState(() => _isDirty = true);
+                  },
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                      labelText: 'Telefone', border: OutlineInputBorder()),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
